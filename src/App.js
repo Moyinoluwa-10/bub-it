e1bc081a9590080470c031bac613950241fc820b
Moyinoluwa-10 <moyinadelowo@gmail.com>
2022-10-25 05:30:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Error from "./Pages/Error/Error";
+import Recent from "./Pages/Recent/Recent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path={"/"} element={<Home />} />
+        <Route exact path={"/recent"} element={<Recent />} />
+        <Route exact path={"/error"} element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
