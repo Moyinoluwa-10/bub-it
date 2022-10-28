634e45e2083d03dd648159a55a0032f7a29fde0f
stephen-gift <79639335+stephen-gift@users.noreply.github.com>
2022-10-28 19:35:21 +0100

    Update Modal.jsx

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -30,7 +30,7 @@ const Modal = ({ closeModal, shortUrl }) => {
         <div className="title">Your Shortened Shrink-URL:</div>
         <div className="body">
           <div className="inputCopy">
-            <input readOnly type="url" value={shortUrl} />
+            <input type="url" value={shortUrl} />
             <CopyToClipboard text={generatedUrl}>
               <button className="copyIcon" onClick={handleCopied}>
                 <TbCopy className="coppy" />
