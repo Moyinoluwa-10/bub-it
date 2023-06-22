91c0753ec5ea4872b869797fb8bf76f112f3b308
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-06-22 17:40:39 +0100

    fix: test 2 to check if deployment works

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,19 @@
+// disable eslint for this file
+/* eslint-disable */
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react-refresh/only-export-components": "warn",
+    "react/prop-types": "off",
   },
-}
+};
+
