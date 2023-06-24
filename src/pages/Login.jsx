29a0ae5daa7c300a8cedfa23c6df77dca862faca
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-06-24 17:02:34 +0100

    feat: production test 9

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -66,6 +66,7 @@ const Login = () => {
           // "Access-Control-Allow-Credentials": true,
           // withCredentials: true,
         },
+        credentials: "include",
         body: JSON.stringify(values),
       })
         .then((response) => response.json())
